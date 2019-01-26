cf3d94f2480f9f89ec4a9f96c78ba9b28a0f2d3f
Juhana Kekoni <juhanakekoni@yahoo.com>
2019-01-26 15:29:52 +0900

    Add StateEngine and MainState
    Preliminary state

---
+++ FruitForce/Assets/States/MainState.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainState : StateMachineBehaviour
+{
+    public bool button = false;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo info, int layerIndex)
+    {
+
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo info, int layerIndex)
+    {
+        Debug.Log("jooh");
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo info, int layerIndex)
+    {
+        
+    }
+}
