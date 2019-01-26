cb4e60b5e8e04c59a3c99c86219cd11455895e43
Timo Nurminen <tialnu@hotmail.com>
2019-01-26 19:52:43 +0200

    Heh

---
+++ FruitForce/Assets/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int playerNum = 1;
+    public string playerController = "c1_";
+    float hInput = 0f;
+    float vInput = 0f;
+    float hInput2 = 0f;
+    float vInput2 = 0f;
+    public float maxVelocity = 4.0f;
+    bool haveGrabbed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        hInput = Input.GetAxis(playerController + "joystick_horizontal");
+        vInput = Input.GetAxis(playerController + "joystick_vertical");
+        hInput2 = Input.GetAxis(playerController + "joystick2_horizontal");
+        vInput2 = Input.GetAxis(playerController + "joystick2_vertical");
+
+        //Debug.Log("hor "+hInput2 +" ver "+vInput2);
+        //Debug.Log(Input.GetButton(playerController + "button_x") + " " + Input.GetButton(playerController + "button_o"));
+        if (Mathf.Sqrt(hInput2 * hInput2 + vInput2 * vInput2) >= 0.3f)
+        {
+            transform.localEulerAngles = new Vector3(0.0f, 0.0f, Mathf.Atan2(vInput2, hInput2) * 180 / Mathf.PI - 90);
+        }
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2(hInput, vInput) * 10);
+        if (rb.velocity.magnitude > maxVelocity)
+        {
+            rb.velocity = rb.velocity.normalized * maxVelocity;
+        }
+
+        if (Input.GetButtonDown(playerController + "button_x"))
+        {
+
+        }
+
+        if (Input.GetButtonDown(playerController + "button_o"))
+        {
+
+        }
+
+        if (Input.GetButtonDown(playerController + "button_R1"))
+        {
+            if (!haveGrabbed)
+            {
+                CircleCollider2D[] results = new CircleCollider2D[3];
+                int num = transform.GetChild(0).GetComponent<CircleCollider2D>().OverlapCollider(new ContactFilter2D(), results);
+                if (num > 0)
+                {
+                    results[0].transform.SetParent(transform.GetChild(0));
+                    results[0].transform.localPosition = Vector3.zero;
+                    results[0].transform.rotation = new Quaternion();
+                    results[0].GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                    results[0].GetComponent<Rigidbody2D>().isKinematic = true;
+                    haveGrabbed = true;
+                }
+
+            }
+            else if (haveGrabbed)
+            {
+                
+                Transform trans = transform.GetChild(0).GetChild(0);
+                trans.parent = null;
+                float angle = (transform.eulerAngles.z+90)/180*Mathf.PI;
+                Debug.Log(angle);
+                Vector2 velocity = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+                trans.GetComponent<Rigidbody2D>().isKinematic = false;
+                trans.GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity + velocity*5;
+
+                
+                haveGrabbed = false;
+            }
+            
+        }
+
+    }
+}
---
+++ FruitForce/Assets/PlayerFactory.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFactory : MonoBehaviour
+{
+
+    public GameObject player;
+    bool[] c;
+    int numPlayer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        c = new bool[4];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("c0_button_x"))
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                if (Input.GetButtonDown("c" + i + "_button_x"))
+                    if (c[i-1] == false)
+                    {
+                        c[i - 1] = true;
+                        numPlayer++;
+                        GameObject newPlayer = Instantiate(player);
+                        newPlayer.GetComponent<PlayerController>().playerController = "c" + i + "_";
+                        newPlayer.GetComponent<PlayerController>().playerNum = numPlayer;
+                    }
+            }
+        }
+    }
+}
