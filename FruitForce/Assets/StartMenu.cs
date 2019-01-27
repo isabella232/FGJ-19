13fb61df6f1b53eb19ae19bd5c648e2366f0b6bc
Juhana Kekoni <juhanakekoni@yahoo.com>
2019-01-27 12:56:36 +0900

    States: Make StartMenu to respond to controller
    Pressing X takes you to GameState

---
+++ FruitForce/Assets/StartMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+    private Animator _state_controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _state_controller = transform.parent.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("c0_button_x"))
+        {
+            _state_controller.SetTrigger("Game");
+        }
+    }
+}
--- FruitForce/Assets/States/State.cs
+++ FruitForce/Assets/States/State.cs
@@ -31,6 +31,5 @@ public static class GameStateExtensions
         }
         return obj;
     }
-
 }
 
