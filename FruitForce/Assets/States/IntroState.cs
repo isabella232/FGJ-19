fcc67f69b3ab5079963b6d357ed3d8b22b2de500
Juhana Kekoni <juhanakekoni@yahoo.com>
2019-01-27 11:14:06 +0900

    States: Transitions between states and refactoring

--- FruitForce/Assets/States/IntroState.cs
+++ FruitForce/Assets/States/IntroState.cs
@@ -1,27 +1,2 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class IntroState : StateMachineBehaviour {
-
-    private GameObject _state = null;
-
-    public override void OnStateEnter(Animator animator, AnimatorStateInfo info, int layerIndex)
-    {
-        _state = animator.GetComponentInChildren<StateEngine>().IntroState;
-
-        // enable intro canvas
-        _state.gameObject.SetActive(true);
-    }
-
-    public override void OnStateExit(Animator animator, AnimatorStateInfo info, int layerIndex)
-    {
-        // disable intro canvas
-        _state.SetActive(false);
-    }
-
-    public override void OnStateUpdate(Animator animator, AnimatorStateInfo info, int layerIndex)
-    {
-        
-    }
-}
+﻿
+public class IntroState : GameState<IntroState> { }
--- FruitForce/Assets/States/StartState.cs
+++ FruitForce/Assets/States/StartState.cs
@@ -1,27 +1,2 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class StartState : StateMachineBehaviour
-{
-    private GameObject _state = null;
-  
-    public override void OnStateEnter(Animator animator, AnimatorStateInfo info, int layerIndex)
-    {
-        _state = animator.GetComponentInChildren<StateEngine>().StartState;
-
-        // enable startmenu canvas
-        _state.gameObject.SetActive(true);
-    }
-
-    public override void OnStateExit(Animator animator, AnimatorStateInfo info, int layerIndex)
-    {
-        // disable startmenu canvas
-        _state.SetActive(false);
-    }
-
-    public override void OnStateUpdate(Animator animator, AnimatorStateInfo info, int layerIndex)
-    {
-
-    }
-}
+﻿
+public class StartState : GameState<StartState> { }
